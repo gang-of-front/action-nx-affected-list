ac8b584c645ab2feb84c6496c975eb0ad77b3828
Guilherme Mangabeira Gregio <guilherme.gregio@creditas.com.br>
2023-05-12 22:14:51 -0300

    feat: change outputs

--- src/main.ts
+++ src/main.ts
@@ -20,8 +20,8 @@ export async function run(workspace = '.'): Promise<void> {
 
     core.debug(`Result: ${result}`)
     core.debug(`Result projects: ${projects}`)
-    core.setOutput('affectedProjects', projects)
-    core.setOutput('hasAffectedProjects', projects.length > 0)
+    core.setOutput('affected', projects)
+    core.setOutput('hasAffected', projects.length > 0)
     core.info(`Affected projects: ${projects.length > 0 ? projects.join() : 'none'}`)
   } catch (error) {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
